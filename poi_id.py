a88ce7ef68b6513c851cd7e1185765da3fd62edc
mtyszler <tyszler.jobs@gmail.com>
2017-09-18 15:24:15 +0200

    Update import statement to current sklearn syntax

--- poi_id.py
+++ poi_id.py
@@ -44,7 +44,8 @@ clf = GaussianNB()
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
+#from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
